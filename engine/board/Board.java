af6b7c9ee7ec966041a30d7a6d7f3b078f0e24d3
Tuanne2108 <107233041+Tuanne2108@users.noreply.github.com>
2023-05-16 14:01:41 +0700

    update Board.java

--- engine/board/Board.java
+++ engine/board/Board.java
@@ -4,4 +4,10 @@ public class Board {
     public Tile getTile(final int tileCoordinate) {
         return null;
     }
+    public Iterable<Move> getAllLegalMoves() {
+        return Iterables.unmodifiableIterable(Iterables.concat(this.whitePlayer.getLegalMoves(), this.blackPlayer.getLegalMoves()));
+    }
+    public void setEnPassantPawn(Pawn enPassantPawn){
+        this.enPassantPawn = enPassantPawn;
+    }
 }
